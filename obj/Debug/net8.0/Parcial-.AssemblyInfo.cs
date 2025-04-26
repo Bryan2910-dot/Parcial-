7c66c60e0378e5e4a62c2d57bb3f95ded2a624bc
Bry <bryan2910arnold@gmail.com>
2025-04-25 22:01:26 -0500

    formularios para jugadores y equipo

--- obj/Debug/net8.0/Parcial-.AssemblyInfo.cs
+++ obj/Debug/net8.0/Parcial-.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Parcial-")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a79a73771fb8a1ac81d77265e497f689c67d063")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69981c8dcbdd438c00cd3d2065d297cd55d2b5ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("Parcial-")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Parcial-")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
